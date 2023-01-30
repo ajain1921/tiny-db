0dad426d388dc7ac577250e650c64ffcf4176e48
AndrewLester <alester220@gmail.com>
2023-01-29 21:09:43 -0600

    Finish logger and node
    Co-authored-by: adityaj5 <adityaj5@illinois.edu>

--- cmd/logger/main.go
+++ cmd/logger/main.go
@@ -1,9 +1,75 @@
 package main
 
 import (
+	"bufio"
+	"errors"
 	"fmt"
+	"math"
+	"net"
+	"os"
+	"strings"
+	"time"
 )
 
 func main() {
-	fmt.Println("Hello from logger!")
+	if err := run(); err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	args := os.Args[1:]
+
+	if len(args) != 1 {
+		return errors.New("exactly 1 command line argument please")
+	}
+
+	port := args[0]
+
+	ln, err := net.Listen("tcp", ":"+port)
+	if err != nil {
+		return err
+	}
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Ruh roh: %v\n", err)
+		}
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	scanner := bufio.NewScanner(conn)
+
+	var nodeName string
+	for scanner.Scan() {
+		line := scanner.Text()
+		split := strings.SplitN(line, " ", 2)
+
+		nodeName = split[0]
+
+		if len(split) == 1 {
+			printConnectionChange(nodeName, "connected")
+			continue
+		}
+
+		splitMessage := strings.Split(split[1], " ")
+
+		fmt.Println(splitMessage[0], nodeName, splitMessage[1])
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintf(os.Stderr, "%v", err)
+	}
+
+	printConnectionChange(nodeName, "disconnected")
+}
+
+func printConnectionChange(nodeName, connectionType string) {
+	now := time.Now()
+	timestamp := fmt.Sprintf("%f", float64(now.UnixNano())/math.Pow(10, 9))
+	fmt.Println(timestamp, "-", nodeName, connectionType)
 }
--- cmd/node/main.go
+++ cmd/node/main.go
@@ -1,9 +1,63 @@
 package main
 
 import (
+	"bufio"
+	"errors"
 	"fmt"
+	"net"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello from node!")
+	if err := run(); err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	args := os.Args[1:]
+
+	if len(args) != 3 {
+		return errors.New("exactly 3 command line arguments please")
+	}
+
+	name := args[0]
+	ip := args[1]
+	port := args[2]
+
+	stdin := bufio.NewReader(os.Stdin)
+
+	var timestamp, hash string
+
+	conn, err := net.Dial("tcp", ip+":"+port)
+	if err != nil {
+		return err
+	}
+	fmt.Fprintln(conn, name)
+	// status, err := bufio.NewReader(conn).ReadString('\n')
+
+	// client, err := rpc.DialHTTP("tcp", ip+":"+port)
+	// if err != nil {
+	// 	return err
+	// }
+
+	// connectData := &logger.ConnectArgs{NodeName: name}
+	// client.Go("Logger.Connect", connectData, nil, nil)
+
+	for {
+		_, err := fmt.Fscanf(stdin, "%s %s\n", &timestamp, &hash)
+		if err != nil {
+			fmt.Println("err: ", err)
+			break
+		}
+
+		// Event := timestamp + " " + hash
+		// logData := &logger.LogArgs{Event: Event, NodeName: name}
+
+		// client.Go("Logger.Log", logData, nil, nil)
+		fmt.Fprintln(conn, name+" "+timestamp+" "+hash)
+	}
+
+	return nil
 }
